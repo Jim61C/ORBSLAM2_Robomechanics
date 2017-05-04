89d03716e1ac7aefbb14c53985318dd34b343e3f
robot <Sharon-Jin@github.com>
2017-05-04 00:04:30 -0400

    add Robomechanics lab wecam caliberation file + scripts for ros node invokation and running

---
+++ mono_web_cam.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+SESSION=ORB_SLAM_MONO_WEB_CAM
+tmux new-session -d -s $SESSION
+
+tmux split-window -h
+tmux select-pane -t 0
+tmux send-keys "roscore" C-m
+#tmux send-keys "ssh kodlab@xrl3" C-m
+sleep 2
+
+tmux split-window -v
+tmux select-pane -t 1
+tmux send-keys "rosrun ORB_SLAM2 Mono Vocabulary/ORBvoc.txt Examples/Monocular/web_cam.yaml" C-m
+# tmux send-keys "echo pane 1 after split-window -v " C-m
+sleep 15
+
+tmux select-pane -t 2
+# tmux send-keys "echo right side " C-m
+tmux send-keys "rosrun image_publisher image_publisher_videocapure_node" C-m
+
+# tmux select-window -t $SESSION
+tmux attach-session -t $SESSION
